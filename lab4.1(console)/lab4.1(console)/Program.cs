c1ca4e4cb29891c2fcbbeca424dcb0e2bf4354d0
awfulbarbie <119601129+awfulbarbie@users.noreply.github.com>
2022-12-06 04:22:05 +0300

    Add files via upload

---
+++ lab4.1(console)/lab4.1(console)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4._1_console_
+{
+    internal class Program
+    {
+        static double recursion (double x, int n) //x в степени n - рекурсия
+        {
+            if (n == 0)
+                return 1;
+            else if (n < 0)
+            {
+               // return 1 / Math.Pow(x, Math.Abs(n));
+               return 1 / recursion (x, Math.Abs (n));
+            }
+            return x * recursion(x, n - 1);
+        }
+        static void Main(string[] args)
+        {
+            
+            int n;
+            double x;
+
+            do
+            {
+                Console.WriteLine("Введите x (вещественное число, больше 0)");
+                if (!double.TryParse(Console.ReadLine(), out x) || x == 0)
+                {
+                    Console.WriteLine("Ошибка! X не является вещественным числом или равен нулю! Повторите попытку!");
+                }
+                else break;
+            } while (true);
+
+            do
+            {
+                Console.WriteLine("Введите n (целое число)");
+                if (!int.TryParse(Console.ReadLine(), out n))
+                {
+                    Console.WriteLine("Ошибка! N не является целым числом! Повторите попытку!");
+                }
+                else break;
+            } while (true);
+
+            Console.WriteLine(recursion(x, n));
+
+        }
+    }
+}
---
+++ lab4.2(console)/lab4.2(console)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4._2_console_
+{
+    internal class Program
+    {
+        static void recursion(int n, int row)
+        {
+            if (row > n)
+                return;
+
+            for (int i = row; i > 0; i--)
+            {
+                Console.Write("  " + i);
+            }
+            Console.WriteLine(" ");
+            recursion(n, row + 1);
+        }
+            static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.Write("Введите n = ");
+                if (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+                {
+                    Console.WriteLine("Ошибка! N не является целым числом или имеет отрицательное или нулевое значение");
+                }
+                else break;
+            } while (true);
+
+            recursion(n, 1);
+        }
+    }
+}
+
